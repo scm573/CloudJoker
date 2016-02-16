88e04c46f9f0f51055ec3c1f8cb04724e50387b5
qifan.wu <qifan.wu@rakuten.com>
2016-02-16 11:01:01 +0900

    Implement the GCE task to pass the joke.

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -0,0 +1,66 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Pair;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.joker.backend.myApi.MyApi;
+import com.udacity.gradle.jokedisplay.JokeActivity;
+import com.udacity.gradle.jokes.Joker;
+
+import java.io.IOException;
+
+/**
+ * Created by qifan.wu on 2016/02/16.
+ */
+public class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+//        try {
+//            return myApiService.sayHi(name).execute().getData();
+//        } catch (IOException e) {
+//            return e.getMessage();
+//        }
+
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+        return joke;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        JokeActivity newJokeActivity = new JokeActivity();
+        Intent intent = new Intent(context, newJokeActivity.getClass());
+        intent.putExtra(JokeActivity.JOKE_KEY, result);
+        context.startActivity(intent);
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.udacity.gradle.jokedisplay.JokeActivity;
-import com.udacity.gradle.jokes.Joker;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -42,11 +40,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Joker joker = new Joker();
-        String joke = joker.getJoke();
-        JokeActivity newJokeActivity = new JokeActivity();
-        Intent intent = new Intent(this, newJokeActivity.getClass());
-        intent.putExtra(JokeActivity.JOKE_KEY, joke);
-        startActivity(intent);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "not use"));
     }
 }
